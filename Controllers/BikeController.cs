0064157a502a5c92bb0eaab5877341376a59d7a1
Victor <victorandhadar@gmail.com>
2024-02-22 15:38:55 +0200

    Functionate Bike" and "sale" nav bar links

--- Controllers/BikeController.cs
+++ Controllers/BikeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BuyBikeShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BuyBikeShop.Controllers
 {
     public class BikeController : Controller
     {
-        public IActionResult Index()
+        private readonly BuyBikeShopContext _context;
+        public BikeController(BuyBikeShopContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var bikes = await _context.Products.Where(p => p.Class_Name == "Bike").ToListAsync();
+
+            return View(bikes);
         }
     }
 }
--- Controllers/SaleController.cs
+++ Controllers/SaleController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BuyBikeShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BuyBikeShop.Controllers
 {
     public class SaleController : Controller
     {
-        public IActionResult Index()
+        private readonly BuyBikeShopContext _context;
+        public SaleController(BuyBikeShopContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var productSale = await _context.Products.Where(p => p.Sale_Perc != 0).ToListAsync();
+
+            return View(productSale);
         }
     }
 }
