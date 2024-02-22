3ce6543080714f50af9ec7837911a153a9bd683a
Victor <victorandhadar@gmail.com>
2024-02-22 21:49:02 +0200

    finish sync all pages on nav bar

--- Controllers/PartController.cs
+++ Controllers/PartController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BuyBikeShop.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BuyBikeShop.Controllers
 {
     public class PartController : Controller
     {
-        public IActionResult Index()
+        private readonly BuyBikeShopContext _context;
+        public PartController(BuyBikeShopContext context) { 
+            _context = context;
+
+        }
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var Parts = await _context.Products.Where(p => p.Class_Name == "Parts").ToListAsync();
+            return View(Parts);
         }
     }
 }
