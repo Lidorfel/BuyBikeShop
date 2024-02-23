312d6f2d102a563377df08eadbe1e1f0bb4f7ded
Mohamad-Aj <117522649+Mohamad-Aj@users.noreply.github.com>
2024-02-23 17:44:04 +0200

    Added checkout controller and the basic views
    added logic to the quantity
    
    added notify me button
    
    activated the buynow button until (sending the quantity and pid to the controller)

---
+++ Controllers/CheckOutController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuyBikeShop.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        [HttpPost]
+        public IActionResult Payment(int quantity,int productId)
+        {
+            ViewBag.Quantity = quantity;
+            ViewBag.Id = productId;
+           
+            return View("Payment",quantity);
+        }
+    }
+}
